0fe8de11af51e726e5b667d096ced641b147bda1
Jason Lin <jasonlin43212@gmail.com>
2020-06-25 08:14:10 -0400

    Updates

---
+++ precache-manifest.e8c243630292465c5875488ecf2cbc58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4305ed7d4b4518647105c39f655cb9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0624fdd69b5c7537fdb",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5242915b016e635c3c86",
+    "url": "/static/js/2.843c732f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.843c732f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0624fdd69b5c7537fdb",
+    "url": "/static/js/main.98dd106b.chunk.js"
+  },
+  {
+    "revision": "0602ed45bc290b296f27",
+    "url": "/static/js/runtime-main.63b07cbf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
