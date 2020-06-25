ac8fba1546ec51be3440d6745062330b602bb798
Alice <ani00@stuy.edu>
2020-06-24 21:35:55 -0400

    frozen flask attempt

--- app.py
+++ app.py
@@ -5,27 +5,27 @@ app = Flask(__name__) # instantiates an instance of Flask
 def home():
     return render_template('index.html')
 
-@app.route("/about")
+@app.route("/about/")
 def about():
     return render_template('about.html')
 
-@app.route("/board")
+@app.route("/board/")
 def board():
     return render_template('board.html')
 
-@app.route("/contact")
+@app.route("/contact/")
 def contact():
     return render_template('contact.html')
 
-@app.route("/photos")
+@app.route("/photos/")
 def photos():
     return render_template('photos.html')
 
-@app.route("/resources")
+@app.route("/resources/")
 def resources():
     return render_template('resources.html')
 
-@app.route("/videos")
+@app.route("/videos/")
 def videos():
     return render_template('videos.html')
 
