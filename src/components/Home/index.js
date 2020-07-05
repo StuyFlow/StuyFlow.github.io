7abf6ce73027dcb5eeda8a3db711e377f028b81d
Jason Lin <jasonlin43212@gmail.com>
2020-07-05 10:47:31 -0400

    snap scrolling

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,8 +5,8 @@ import './Home.css';
 class Home extends Component {
     render() {
         return (
-            <div>
-                <div>
+            <div className="homepage">
+                <div className="home-div video">
                     <video autoPlay muted loop className="backgroundVideo">
                         <source src={backgroundVideo} type="video/mp4"/>
                     </video>
@@ -15,8 +15,14 @@ class Home extends Component {
                         <div className="slogan">Stars Shine Brightest in the Dark</div>
                     </div>
                 </div>
-                <div className="page">
+                <div className="about-page home-div">
                     <div className="pageHeader">What is StuyFlow?</div>
+                    <div className="about-desc">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vivamus at augue eget arcu dictum varius duis at. Egestas sed sed risus pretium quam vulputate. Commodo viverra maecenas accumsan lacus vel. Et pharetra pharetra massa massa ultricies mi quis hendrerit. Nullam non nisi est sit amet. Turpis egestas maecenas pharetra convallis posuere morbi leo urna. Adipiscing tristique risus nec feugiat in fermentum posuere urna. Sed ullamcorper morbi tincidunt ornare. Fermentum odio eu feugiat pretium nibh. Tincidunt dui ut ornare lectus sit amet. Aliquet porttitor lacus luctus accumsan tortor posuere ac ut. Ut tortor pretium viverra suspendisse potenti nullam ac tortor. Cursus vitae congue mauris rhoncus aenean vel elit scelerisque mauris. Sed blandit libero volutpat sed cras ornare arcu dui vivamus. Est pellentesque elit ullamcorper dignissim cras tincidunt. Enim nec dui nunc mattis enim ut tellus. Mattis vulputate enim nulla aliquet porttitor lacus luctus.
+                    </div>
+                    <div className="about-desc">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vivamus at augue eget arcu dictum varius duis at. Egestas sed sed risus pretium quam vulputate. Commodo viverra maecenas accumsan lacus vel. Et pharetra pharetra massa massa ultricies mi quis hendrerit. Nullam non nisi est sit amet. Turpis egestas maecenas pharetra convallis posuere morbi leo urna. Adipiscing tristique risus nec feugiat in fermentum posuere urna. Sed ullamcorper morbi tincidunt ornare. Fermentum odio eu feugiat pretium nibh. Tincidunt dui ut ornare lectus sit amet. Aliquet porttitor lacus luctus accumsan tortor posuere ac ut. Ut tortor pretium viverra suspendisse potenti nullam ac tortor. Cursus vitae congue mauris rhoncus aenean vel elit scelerisque mauris. Sed blandit libero volutpat sed cras ornare arcu dui vivamus. Est pellentesque elit ullamcorper dignissim cras tincidunt. Enim nec dui nunc mattis enim ut tellus. Mattis vulputate enim nulla aliquet porttitor lacus luctus.
+                    </div>
                     <div className="about-desc">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vivamus at augue eget arcu dictum varius duis at. Egestas sed sed risus pretium quam vulputate. Commodo viverra maecenas accumsan lacus vel. Et pharetra pharetra massa massa ultricies mi quis hendrerit. Nullam non nisi est sit amet. Turpis egestas maecenas pharetra convallis posuere morbi leo urna. Adipiscing tristique risus nec feugiat in fermentum posuere urna. Sed ullamcorper morbi tincidunt ornare. Fermentum odio eu feugiat pretium nibh. Tincidunt dui ut ornare lectus sit amet. Aliquet porttitor lacus luctus accumsan tortor posuere ac ut. Ut tortor pretium viverra suspendisse potenti nullam ac tortor. Cursus vitae congue mauris rhoncus aenean vel elit scelerisque mauris. Sed blandit libero volutpat sed cras ornare arcu dui vivamus. Est pellentesque elit ullamcorper dignissim cras tincidunt. Enim nec dui nunc mattis enim ut tellus. Mattis vulputate enim nulla aliquet porttitor lacus luctus.
                     </div>
