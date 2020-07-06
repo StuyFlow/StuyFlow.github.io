e0ddd286867a4eca4995bf2f0df90ad7918c83e9
Jason Lin <jasonlin43212@gmail.com>
2020-07-05 21:34:53 -0400

    made year change

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -3,28 +3,53 @@ import { board } from './board';
 import './Board.css';
 
 class Board extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            year: "2020-2021",
+        }
+    }
+
+    changeYear = e => {
+        this.setState({ year: e.target.value });
+    }
+
     render() {
+        const members = board[this.state.year];
         return (
             <div className="page">
-                <div className="pageHeader">StuyFlow Board</div>
-                {board.map(term => (
-                    <div key={term.year} style={{color:"white"}}>
-                        <h2>{term.year}</h2>
-                        {term.people.map(person => (
-                            <div>
-                                <img
-                                    className="board-photos"
-                                    src={require(`./imgs/${term.year}/${person.imgName}`)}
-                                />
-                                <br/>
-                                {person.name} <br/>
-                                {person.title} <br/>
-                                {person.desc}
-                                <br/> <br/>
-                            </div>
+                <div className="pageHeader">StuyFlow Board Members</div>
+                <div className="year-selector">
+                    <div className="year-label">Year:</div>
+                    <select onChange={this.changeYear}>
+                        {Object.keys(board).map(term => (
+                            <option key={term}>
+                                {term}
+                            </option>
                         ))}
-                    </div>
-                ))}
+                    </select>
+                </div>
+                <div className="board-members">
+                    {members.map(person => (
+                        <div className="person">
+                            <img
+                                className="board-photo"
+                                src={require(`./imgs/${this.state.year}/${person.imgName}`)}
+                            />
+                            <div className="info">
+                                <div>
+                                    {person.name}
+                                </div>
+                                <div>
+                                    {person.title}
+                                </div>
+                                <div>
+                                    {person.desc}
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
         );
     }
