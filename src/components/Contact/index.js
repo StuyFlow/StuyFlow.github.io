2b21789fcd9a0bcfd5807155734e74d66f123b39
swang01 <52456937+swang01@users.noreply.github.com>
2020-07-06 17:42:20 -0400

    Added links to contact page. Working on email form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -6,9 +6,44 @@ class Contact extends Component {
         return (
             <div className="page">
                 <div className="pageHeader">Contact Us</div>
-                <div>
-                    Contact us here (links here)!!!
-                </div>
+                <div className="sentence">
+                <br></br>
+                    Click on the links below to check us out on our social media!
+                    </div>
+                    <div className="a">
+                      <a href="https://www.youtube.com/channel/UCsWvNNE26HT40Tk7O9afkSA">
+                        <br></br>
+                          Youtube
+                      </a>
+                    </div>
+                    <div className="a">
+                      <a href="https://www.instagram.com/stuyflow/">
+                        <br></br>
+                          Instagram: @stuyflow
+                      </a>
+                    </div>
+                    <div className="a">
+                      <a href="https://www.tiktok.com/@stuyflow?lang=en">
+                        <br />
+                          TikTok: @stuyflow
+                      </a>
+                    </div>
+                    <div className="sentence">
+                        <a href="mailto:stuyvesantflow@gmail.com?subject=test%20email"><br></br>If you would like to contact us, please send an email to stuyvesantflow@gmail.com</a>
+
+                    </div>
+                    <div>
+                    <form action="mailto:someone@example.com" method="post" enctype="text/plain" />
+Name:<br />
+<input type="text" name="name" /><br />
+E-mail:<br />
+<input type="text" name="mail" /><br />
+Comment:<br/ >
+<input type="text" name="comment" size="50" /><br />
+<input type="submit" value="Send" />
+<input type="reset" value="Reset" />
+                    </div>
+
             </div>
         );
     }
