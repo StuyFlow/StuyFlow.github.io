8a927ca1384523475d99019042e57fee7c43ed04
Jason Lin <jasonlin43212@gmail.com>
2020-07-06 19:26:20 -0400

    updated about

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -17,24 +17,24 @@ class Home extends Component {
                 </div>
                 <div className="about-page home-div">
                     <div className="pageHeader">What is StuyFlow?</div>
-                    <div>
+                    <div className="about-desc">
                         StuyFlow is a highschool club based in New York City
-                        focused on mastering props to create shows. The props
-                        we use include: poi, whips, staffs, and fans. We
-                        teach members how to use this variety to perform in
-                        any of three school shows--StuySquad, SING!, and SOS.
+                        focused on learning how to use props to create beautiful
+                        shows.
+                        We teach members how to use a variety of props
+                        like poi, whips, staffs, and fans to perform in
+                        any of three school shows — StuySquad, SING!, and SOS.
                         We operate on a set of principles known as PLURR,
                         partly due to the large culmulative effort needed to
                         run shows smoothly. This motto stands for “Peace,
                         Love, Unity, Respect, and Responsibility”.
                     </div>
-                    <div>
+                    <div className="about-desc">
                         During off-seasons, a.k.a. times when we don’t
                         have shows, we meet on Wednesdays and Fridays,
-                        usually in room 736 or 738. These meetings are
-                        used to get to know each other better, learn new
-                        moves, create choreography projects, etc. We use
-                        choreography projects as stress-free creative outlets,
+                        usually in rooms 736 or 738. In these meetings, learn new
+                        moves, get to know each other better, create choreography projects, etc.
+                        We use choreography projects as stress-free creative outlets,
                         as they are not for shows, and they are displayed at a
                         studio, typically during July or August.
                     </div>
