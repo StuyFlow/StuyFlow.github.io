3a09fa38a06e1ce774cea4bccbb99d890b0d4899
swang01 <52456937+swang01@users.noreply.github.com>
2020-07-06 22:56:32 -0400

    finished contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,12 +1,52 @@
 import React, { Component } from 'react';
 import './Contact.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faFacebookSquare,
+  faTwitter,
+  faLinkedin,
+  faSkype,
+  faGoogle,
+  faInstagramSquare,
+  faYoutube,
+} from '@fortawesome/free-brands-svg-icons'
+
 
 class Contact extends Component {
     render() {
         return (
-            <div className="page">
+             <div className="page">
                 <div className="pageHeader">Contact Us</div>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css" />
+                <script src="https://kit.fontawesome.com/index.js"></script>
+
+             <div className="sentence">
+                <br></br>
+                If you would like to contact us, please send an email to&nbsp;
+                    <a href="mailto:stuyvesantflow@gmail.com?subject=test%20email"><br></br>stuyvesantflow@gmail.com</a>
+                    <br></br>
+
+                </div>
+                <br></br>
                 <div className="sentence">
+                  <br></br>
+                    Click the icons below to check us out on our social media!
+                </div>
+                <br></br>
+
+                <a href="https://www.facebook.com/StuyFl0w" class="fab fa-facebook-square" style={{fontSize: '40px'}}></a>&nbsp;&nbsp;&nbsp;
+                <a href="https://www.youtube.com/channel/UCsWvNNE26HT40Tk7O9afkSA" class="fab fa-youtube" style={{fontSize: '38px'}}></a>&nbsp;&nbsp;&nbsp;
+                <a href="https://www.instagram.com/stuyflow/" class="fab fa-instagram" style={{fontSize: '40px'}}></a>&nbsp;&nbsp;&nbsp;
+                <a href="https://www.tiktok.com/@stuyflow?lang=en" class="fab fa-tiktok"style={{fontSize: '40px'}}></a>
+
+                <div className="sentence">
+                   <br></br>
+                   Private Facebook Group (Stuyvesant Students Only!)
+                </div>
+                   <br></br>
+                         <a href="https://www.facebook.com/groups/277759659004764" class="fab fa-facebook-square" style={{fontSize: '40px'}}></a>
+
+                {/* <div className="sentence">
                 <br></br>
                     Click on the links below to check us out on our social media!
                     </div>
@@ -16,6 +56,7 @@ class Contact extends Component {
                           Youtube
                       </a>
                     </div>
+
                     <div className="a">
                       <a href="https://www.instagram.com/stuyflow/">
                         <br></br>
@@ -28,22 +69,17 @@ class Contact extends Component {
                           TikTok: @stuyflow
                       </a>
                     </div>
-                    <div className="sentence">
-                        <a href="mailto:stuyvesantflow@gmail.com?subject=test%20email"><br></br>If you would like to contact us, please send an email to stuyvesantflow@gmail.com</a>
-
+                    <div className="a">
+                      <a href="https://www.facebook.com/StuyFl0w"><br></br>
+                          Public Facebook page
+                      </a>
                     </div>
-                    <div>
-                    <form action="mailto:someone@example.com" method="post" enctype="text/plain" />
-Name:<br />
-<input type="text" name="name" /><br />
-E-mail:<br />
-<input type="text" name="mail" /><br />
-Comment:<br/ >
-<input type="text" name="comment" size="50" /><br />
-<input type="submit" value="Send" />
-<input type="reset" value="Reset" />
-                    </div>
-
+                    <div className="a">
+                      <a href="https://www.facebook.com/groups/277759659004764">
+                        <br />
+                          Private Facebook group (Stuyvesant Students/Alumni only!)
+                      </a>
+                    </div> */}
             </div>
         );
     }
