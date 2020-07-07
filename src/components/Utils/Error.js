044c5f29c4b94498c7e6be575a8cdedbd5d82d08
Jason Lin <jasonlin43212@gmail.com>
2020-07-07 14:35:22 -0400

    started work on animation

--- src/components/Utils/Error.js
+++ src/components/Utils/Error.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react';
 import './Error.css';
 
+const startAnimation = () => {
+    const c = document.getElementById('canvas');
+    const ctx = c.getContext("2d");
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+    ctx.fillStyle = "#ffffff";
+    ctx.beginPath();
+    ctx.ellipse(100,100,50,50,0,0,Math.PI*2);
+    ctx.fill();
+};
+
+
 class Error extends Component {
+    componentDidMount() {
+        startAnimation();
+    }
+
     render() {
         return (
             <div className="page error">
@@ -12,6 +29,9 @@ class Error extends Component {
                     You are seeing this page because the url does not exist.
                     Please check it for spelling errors.
                 </div>
+                <canvas width={400} height={400} className="canvas" id="canvas">
+
+                </canvas>
             </div>
         );
     }
