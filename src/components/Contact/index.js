0d9f85e5a0596298ebb46f66ad915b824b6c7db6
swang01 <52456937+swang01@users.noreply.github.com>
2020-07-07 23:17:01 -0400

    hopefully final revisions for contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -14,7 +14,7 @@ class Contact extends Component {
              <div className="sentence">
                 <div className="break">
                 If you would like to contact us, please send an email to&nbsp;
-                    <a href="mailto:stuyvesantflow@gmail.com?subject=test%20email">stuyvesantflow@gmail.com</a>!
+                    <a href="mailto:stuyvesantflow@gmail.com?">stuyvesantflow@gmail.com</a>!
                     </div>
 
                 </div>
@@ -25,21 +25,21 @@ class Contact extends Component {
                 </div>
                 </div>
 
-                <a href="https://www.facebook.com/StuyFl0w" className="fab fa-facebook-square" target='_blank'></a>
-                <a href="https://www.youtube.com/channel/UCsWvNNE26HT40Tk7O9afkSA" className="fab fa-youtube target='_blank'" ></a>
-                <a href="https://www.instagram.com/stuyflow/" className="fab fa-instagram" target='_blank' ></a>
-                <a href="https://www.tiktok.com/@stuyflow?lang=en" className="fab fa-tiktok" target='_blank'></a>
+                <a href="https://www.facebook.com/StuyFl0w" className="fab fa-facebook-square" target='_blank' rel="noopener noreferrer"></a>
+                <a href="https://www.youtube.com/channel/UCsWvNNE26HT40Tk7O9afkSA" className="fab fa-youtube" target='_blank' rel="noopener noreferrer"></a>
+                <a href="https://www.instagram.com/stuyflow/" className="fab fa-instagram" target='_blank' rel="noopener noreferrer"></a>
+                <a href="https://www.tiktok.com/@stuyflow?lang=en" className="fab fa-tiktok" target='_blank' rel="noopener noreferrer"></a>
 
                 <div className="sentence">
                    <div className="break">
                    Private Facebook Group (Stuyvesant Students Only)!
                 </div>
                    </div>
-                         <a href="https://www.facebook.com/groups/277759659004764" class="fab fa-facebook-square" target='_blank' ></a>
+                         <a href="https://www.facebook.com/groups/277759659004764" class="fab fa-facebook-square" target='_blank' rel="noopener noreferrer" ></a>
                          <div className="sentence">
                          <div className="break">
                          For more information about our club, visit us at&nbsp;
-                      <a href="https://www.stuyactivities.org/club/stuyflow" target='_blank'>
+                      <a href="https://www.stuyactivities.org/club/stuyflow" target='_blank' rel="noopener noreferrer">
                           StuyActivities
                       </a>.
                       </div>
