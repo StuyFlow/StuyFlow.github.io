f64e657dc347af131a2614ffd160171d4868b29b
Jason Lin <jasonlin43212@gmail.com>
2020-07-07 22:49:02 -0400

    separate components

--- src/components/Photos/index.js
+++ src/components/Photos/index.js
@@ -29,45 +29,46 @@ const sorts = [ "Date Asc.", "Date Desc." ];
 
 class Modal extends Component {
     render () {
-	return (
+        console.log(this.props);
+    	return (
 	    	<div id="myModal" className="modal">
-    		<span
-    	    className="close"
-    	    onClick={() => document.getElementById("myModal").style.display = 'none'}
-    		>&times;</span>
-    		<div id="image-title"></div>
-    		<div style={{display: "flex"}}>
-    		<img className="modal-content" id="big-image"/>
-    		<p id="image-credits"></p>
-    		<div id="image-desc"></div>
-    		<a id="download" download></a>
+        		<span
+            	    className="close"
+            	    onClick={() => this.props.setModalImg(null)}
+        		>
+                    &times;
+                </span>
+        		<div id="image-title">{this.props.img.title}</div>
+        		<div style={{display: "flex"}}>
+            		<img
+                        className="modal-content"
+                        id="big-image"
+                        src={require(`./imgCategories/${this.props.img.category}/${this.props.img.imgname}`)}
+                    />
+            		<p id="image-credits">Photo Credits: {this.props.img.credits}</p>
+            		<div id="image-desc">{this.props.img.desc}</div>
+            		<a
+                        id="download"
+                        href={require(`./imgCategories/${this.props.img.category}/${this.props.img.imgname}`)}
+                        download
+                    >
+                        Download
+                    </a>
+        		</div>
     		</div>
-    		</div>
-	)
-    
+    	);
     }
 }
 
 class Image extends Component {
     render() {
-	return (
-	    <React.Fragment>
-	    <Modal />
+    	return (
     		<img
                 className="imag"
-        	    onClick={() => {
-        		document.getElementById("big-image").setAttribute('src', require('./imgCategories/' + this.props.category + '/' + this.props.imgname))
-        		document.getElementById("image-desc").innerHTML = this.props.desc
-        		document.getElementById("image-title").innerHTML = this.props.title
-        		document.getElementById("download").innerHTML = 'Download'
-        		document.getElementById("download").setAttribute("href", './imgCategories/' + this.props.category + '/' + this.props.imgname)
-        		document.getElementById("image-credits").innerHTML = "Photo Credits: " + this.props.credits
-        		document.getElementById("myModal").style.display = 'block'
-        	    }}
-        	    src={require('./imgCategories/' + this.props.category + '/' + this.props.imgname)}
+                src={require(`./imgCategories/${this.props.category}/${this.props.imgname}`)}
+                onClick={() => this.props.setModalImg(this.props)}
             />
-		</React.Fragment>
-	)
+        );
     }
 }
 
@@ -81,48 +82,54 @@ const processImages = (imgs, category) => {
     });
 };
 
-const get_photos = (category, sort) => {
-    let dates = [];
-    if (category === "All") {
-    	let c = Object.keys(categories);
-    	c.shift();
-        for (let aCategory of c) {
-            dates = [...dates, ...processImages(categories[aCategory], aCategory)];
-        }
-    } else {
-    	dates = [...processImages(categories[category], category)];
-    }
-    if (sort === "Date Desc.") {
-        dates.sort((a, b) => a.date - b.date);
-    } else {
-        dates.sort((a, b) => b.date - a.date);
-    }
-    return dates.map(img => (
-        <Image
-            desc={ img.desc }
-            title={ img.name }
-            category={ img.category }
-            imgname={img.imgname}
-            credits={img.credits}
-        />
-    ));
-}
-
 class Photos extends Component {
     constructor(props) {
         super(props);
         this.state = {
             category: "All",
 	        sort: "Date Asc.",
+            modalImg: null,
         };
     }
 
+    get_photos = (category, sort) => {
+        let dates = [];
+        if (category === "All") {
+        	let c = Object.keys(categories);
+        	c.shift();
+            for (let aCategory of c) {
+                dates = [...dates, ...processImages(categories[aCategory], aCategory)];
+            }
+        } else {
+        	dates = [...processImages(categories[category], category)];
+        }
+        if (sort === "Date Desc.") {
+            dates.sort((a, b) => a.date - b.date);
+        } else {
+            dates.sort((a, b) => b.date - a.date);
+        }
+        return dates.map(img => (
+            <Image
+                desc={ img.desc }
+                title={ img.name }
+                category={ img.category }
+                imgname={img.imgname}
+                credits={img.credits}
+                setModalImg={this.setModalImg}
+            />
+        ));
+    }
+
+    setModalImg = modalImg => {
+        this.setState({ modalImg });
+    };
+
     changeCategory = category => {
-        this.setState({ category: category });
+        this.setState({ category });
     };
 
     changeSort = e => {
-	this.setState({ sort: e.target.value });
+        this.setState({ sort: e.target.value });
     };
 
     render() {
@@ -150,8 +157,9 @@ class Photos extends Component {
                 </div>
        		</div>
     		<div className="images">
-    		{ get_photos(this.state.category, this.state.sort) }
+    		{ this.get_photos(this.state.category, this.state.sort) }
     	    </div>
+            { this.state.modalImg && <Modal img={this.state.modalImg} setModalImg={this.setModalImg}/> }
         </div>
         );
     }
