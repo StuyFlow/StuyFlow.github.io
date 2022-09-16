97b067b139e08268c1e34c8166f98ae285514906
Aaron Hsu <aaron.hsu@duke.edu>
2022-09-16 00:04:30 -0400

    changed default board page to 2022-2023

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -7,7 +7,7 @@ class Board extends Component {
     constructor(props){
         super(props);
         this.state = {
-            year: "2021-2022",
+            year: "2022-2023",
         }
     }
 
